fdf47ab8c1e6a58caa4c33e627eb5c9babb33290
milkness <dfhilder@gmail.com>
2020-10-01 00:45:29 +0900

    stream load

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String name;
-
   bool _initialized = false;
   bool _error = false;
-  List<DocumentSnapshot> doclist = [];
 
   @override
   void initState() {
@@ -30,7 +27,7 @@ class _MyAppState extends State<MyApp> {
       await Firebase.initializeApp();
       setState(() {
         _initialized = true;
-        loadFirebase();
+        //    loadFirebase();
       });
     } catch (e) {
       // Set `_error` stat7e to true if Firebase initialization fails
@@ -40,34 +37,108 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  loadFirebase() {
-    final col = FirebaseFirestore.instance.collection('chat');
-    col.get().then((value) {
-      Map<int, DocumentSnapshot> map2 = value.docs.asMap();
-      doclist = map2.values.toList();
-      print(doclist[0].data()['name']);
-      print(doclist[1].data()['name']);
-      print(doclist.length);
-      setState(() {});
-    });
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'firechat', home: ChatScreen());
   }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  List<QueryDocumentSnapshot> doclist = [];
+
+  TextEditingController sendController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'firechat',
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('firechat'),
-          ),
-          body: ListView.builder(
-            shrinkWrap: true,
-            itemCount: doclist.length,
-            itemBuilder: (context, index) {
-              return Text(doclist[doclist.length - 1 - index].data()['name'] +
-                  doclist[doclist.length - 1 - index].data()['message']);
-            },
-          )),
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('firechat'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: messageLine(),
+            ),
+            Divider(
+              thickness: 1,
+            ),
+            bottomInputLine(width, height)
+          ],
+        ));
+  }
+
+  Widget bottomInputLine(width, height) {
+    return Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50), color: Colors.black12),
+        height: 40,
+        width: width * 1,
+        margin:
+            EdgeInsets.only(left: width * 0.03, right: width * 0.03, bottom: 7),
+        padding: EdgeInsets.only(left: width * 0.015, right: width * 0.015),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 9,
+              child: TextField(
+                controller: sendController,
+                textAlign: TextAlign.left,
+                decoration: InputDecoration.collapsed(hintText: '대화를 입력하세요.'),
+                textInputAction: TextInputAction.done,
+                onSubmitted: (value) {
+                  _sendMessage(sendController.text, '유상');
+                },
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.send),
+              color: Colors.blue,
+              onPressed: () {},
+            )
+          ],
+        ));
+  }
+
+  Widget messageLine() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: Text('채팅이 없습니다.'),
+          );
+        }
+        doclist = snapshot.data.docs; //list<docs>
+        return ListView.builder(
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return _buildReciveMessage(doclist[doclist.length - 1 - index]);
+          },
+          itemCount: doclist.length,
+        );
+      },
     );
+  }
+
+  Widget _buildReciveMessage(doc) {
+    return Container(
+      child: Text(doc.data()['name'] + ' ' + doc.data()['message']),
+    );
+  }
+
+  void _sendMessage(message, userid) {
+    FirebaseFirestore.instance
+        .collection('chat')
+        .add({'name': userid, 'message': message});
+    sendController.clear();
   }
 }
